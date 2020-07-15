e49c1e0844ac7f03e96c2219bd5ec032870ca09b
Giampiero Specogna <giampi_12@hotmail.com>
2020-07-15 09:12:00 -0400

    Exclude prod js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,8 @@
-module.exports = {
+let keys = {};
+if(process.env.NODE_ENV !== 'production')
+    keys = require('./dev');
+if(process.env.NODE_ENV === 'production')
+    kyes = require('./prod');
 
-};
+    
+module.exports = keys;
