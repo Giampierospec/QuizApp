d206c001fb26a1be36f3df81a308372870b38eb2
Giampiero Specogna <giampi_12@hotmail.com>
2020-08-03 09:59:32 -0400

    modified port variable

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cookieSession({
 }));
 
 app.use('/api',usersRoutes,quizzesRoutes);
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV === 'production'){
     // Express will serve Production Assets
