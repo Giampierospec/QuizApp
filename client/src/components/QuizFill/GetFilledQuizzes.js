5df4f906640de8ee6083ad04edd69f657cb239a5
Giampiero Specogna <giampi_12@hotmail.com>
2020-08-10 12:28:18 -0400

    Styling some component

--- client/src/components/QuizFill/GetFilledQuizzes.js
+++ client/src/components/QuizFill/GetFilledQuizzes.js
@@ -3,18 +3,20 @@ import { connect } from 'react-redux';
 import { getFilledQuizzes, deleteFilledQuiz } from '../../actions';
 import Loading from '../Loading';
 import { Link } from 'react-router-dom';
-import  DeleteModal from './ModalDelete';
+import DeleteModal from './ModalDelete';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 class GetFilledQuizzes extends Component {
-    state = { loading: true, showDelete:false, quiz:{} }
+    state = { loading: true, showDelete: false, quiz: {} }
     async componentDidMount() {
         await this.props.getFilledQuizzes();
         this.setState({ loading: false });
     }
-    showDeleteModal= (quiz)=>{
-        this.setState({showDelete:true,quiz});
+    showDeleteModal = (quiz) => {
+        this.setState({ showDelete: true, quiz });
     }
-    onClose = ()=>{
-        this.setState({showDelete:false});
+    onClose = () => {
+        this.setState({ showDelete: false });
     }
     renderList() {
         return this.props.quizFill.map((quiz, i) => {
@@ -30,7 +32,7 @@ class GetFilledQuizzes extends Component {
                             </div>
                             <div className="card-footer">
                                 <Link className="btn btn-info float-left" to={`/filled/${quiz._id}`}>Detail</Link>
-                                <button className="btn btn-danger float-right" onClick={()=>{this.showDeleteModal(quiz)}}>Delete</button>
+                                <button className="btn btn-danger float-right" onClick={() => { this.showDeleteModal(quiz) }}><FontAwesomeIcon icon={faTrash} /></button>
                             </div>
                         </div>
                     </div>
@@ -40,15 +42,23 @@ class GetFilledQuizzes extends Component {
         });
     }
     renderContent() {
-        return (
-            <div>
-                <div className="margin-from-top">
-                    <h4 className="text-center">Quizzes Filled By {this.props.auth.name}</h4>
-                    <hr />
-                    {this.renderList()}
+        if (!this.props.quizFill.length)
+            return (<div className="margin-from-top row text-center">
+                <div className="col-sm-8 offset-sm-2">
+                    <h5>Nothing to show at the moment</h5>
+                    <Link to="/quizFill" className="btn btn-secondary">Fill some quizzes</Link>
                 </div>
-                <DeleteModal show={this.state.showDelete} quiz={this.state.quiz} close={this.onClose}/>
-            </div>);
+            </div>)
+        else
+            return (
+                <div>
+                    <div className="margin-from-top">
+                        <h4 className="text-center">Quizzes Filled By {this.props.auth.name}</h4>
+                        <hr />
+                        {this.renderList()}
+                    </div>
+                    <DeleteModal show={this.state.showDelete} quiz={this.state.quiz} close={this.onClose} />
+                </div>);
     }
     render() {
         if (this.state.loading)
@@ -57,5 +67,5 @@ class GetFilledQuizzes extends Component {
             return this.renderContent()
     }
 }
-const mapStateToProps = ({ quizFill , auth}) => ({ quizFill, auth });
+const mapStateToProps = ({ quizFill, auth }) => ({ quizFill, auth });
 export default connect(mapStateToProps, { getFilledQuizzes, deleteFilledQuiz })(GetFilledQuizzes);
