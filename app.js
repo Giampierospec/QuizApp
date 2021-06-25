b6e2b9e1b24e21b6c79a919abd46995e71b95920
Giampiero Specogna <giampi_12@hotmail.com>
2021-06-25 19:34:57 -0400

    changes

--- app.js
+++ app.js
@@ -26,4 +26,4 @@ if(process.env.NODE_ENV === 'production'){
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     });
 }
-app.listen(PORT,()=> console.log(`App started on port ${PORT}`));
+app.listen(PORT,()=> console.log(`App started on port ${PORT}!`));
