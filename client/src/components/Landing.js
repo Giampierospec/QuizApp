a792b20a8f82f7a7246978be080c48e38846c351
Giampiero Specogna <giampi_12@hotmail.com>
2022-04-25 20:42:00 -0400

    fix: landing

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
-class Landing extends Component{
-    render(){
-        return(
-            <div className="row margin-from-top">
-                <div className="col-sm-8 offset-sm-2">
-                    <div className="card text-center">
-                        <div className="card-body">
-                           <h4 className="card-title"> Welcome!</h4>
-                            <hr/>
-                            <p className="card-text">
-                                This App is a practice to Make some dynamic quizzes and answering them
-                            </p>
-                        </div>
-                        <div className="card-footer">
-                            <Link disabled={this.props.auth.role !== 'admin'} to='/create' className="btn btn-primary">create Quiz</Link>
-                        </div>
-                    </div>
-                </div>
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+class Landing extends Component {
+  render() {
+    return (
+      <div className="row margin-from-top">
+        <div className="col-sm-8 offset-sm-2">
+          <div className="card text-center">
+            <div className="card-body">
+              <h4 className="card-title"> Welcome!</h4>
+              <hr />
+              <p className="card-text">
+                This App is a practice to Make some dynamic quizzes and
+                answering them
+              </p>
             </div>
-        )
-    }
+            <div className="card-footer">
+              <Link
+                disabled={this.props.auth.role !== 'admin'}
+                to="/create"
+                className="btn btn-primary"
+              >
+                create Quiz
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="klaviyo-form-TKYgfy"></div>
+      </div>
+    )
+  }
 }
 
-const mapStateToProps = ({auth})=> ({auth});
-export default connect(mapStateToProps)(Landing);
+const mapStateToProps = ({ auth }) => ({ auth })
+export default connect(mapStateToProps)(Landing)
